01db8191de34fe21f28fddcf8eaa6b8c5b69d393
Scott Brady <scottbrady91@outlook.com>
2017-10-26 12:59:56 +0100

    Initial working version using Sodium.Core. Options TODO

---
+++ src/ScottBrady91.AspNetCore.Identity.ScryptPasswordHasher/ScryptPasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Scrypt;
+
+namespace ScottBrady91.AspNetCore.Identity
+{
+    public class ScryptPasswordHasher<TUser> : IPasswordHasher<TUser> where TUser : class
+    {
+        public string HashPassword(TUser user, string password)
+        {
+            if (password == null) throw new ArgumentNullException(nameof(password));
+
+            var encoder = new ScryptEncoder();
+            return encoder.Encode(password);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+        {
+            if (hashedPassword == null) throw new ArgumentNullException(nameof(hashedPassword));
+            if (providedPassword == null) throw new ArgumentNullException(nameof(providedPassword));
+
+            var encoder = new ScryptEncoder();
+            var isValid = encoder.Compare(providedPassword, hashedPassword);
+
+            return isValid ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+        }
+    }
+}
---
+++ test/ScottBrady91.AspNetCore.Identity.ScryptPasswordHasher.Tests/ScryptPasswordHasherTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Scrypt;
+using Xunit;
+
+namespace ScottBrady91.AspNetCore.Identity.ScryptPasswordHasher.Tests
+{
+    public class ScryptPasswordHasherTests
+    {
+        [Fact]
+        public void HashPassword_WithDefaultSettings_ExpectVerifiableHash()
+        {
+            var password = Guid.NewGuid().ToString();
+
+            var hasher = new ScryptPasswordHasher<string>();
+            var hashedPassword = hasher.HashPassword("", password);
+
+            var encoder = new ScryptEncoder();
+            encoder.Compare(password, hashedPassword).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void VerifyHashedPassword_WithDefaultSettings_ExpectSuccess()
+        {
+            var password = Guid.NewGuid().ToString();
+            var encoder = new ScryptEncoder();
+            var hashedPassword = encoder.Encode(password);
+
+            var hasher = new ScryptPasswordHasher<string>();
+
+            hasher.VerifyHashedPassword("", hashedPassword, password).Should().Be(PasswordVerificationResult.Success);
+        }
+        
+        [Fact]
+        public void VerifyHashedPassword_WhenSuppliedPasswordDoesNotMatch_ExpectFailure()
+        {
+            var password = Guid.NewGuid().ToString();
+            var encoder = new ScryptEncoder();
+            var hashedPassword = encoder.Encode(Guid.NewGuid().ToString());
+
+            var hasher = new ScryptPasswordHasher<string>();
+
+            hasher.VerifyHashedPassword("", hashedPassword, password).Should().Be(PasswordVerificationResult.Failed);
+        }
+    }
+}
